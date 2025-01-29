cd1d12e8311785eac63669bdfd23f5dfa3c43c17
LostKosmonaut26 <maxon-master@yandex.ru>
2025-01-29 10:47:00 +0300

    ReadyApp

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ManagmentPersonal
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PersonalForm());
         }
     }
 }
